6897f0afad69ceb0ee405d91664f1b44f7b9cb3b
Daanish Hindustani <134811343+Daanish-Hindustani@users.noreply.github.com>
2026-04-06 16:45:42 -0500

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+
+        for word in strs:
+            length = len(word)
+            res += str(length) + "<s>" + word
+        
+        return res
+    def decode(self, s: str) -> List[str]:
+        
+        res = []
+
+        i = 0
+
+        while i < len(s):
+
+            j = i
+
+            while s[j] != "<":
+                j += 1
+            
+            length = int(s[i:j])
+
+            word = s[j+3: j+3+length]
+
+            res.append(word)
+
+            i = j + 3 + length
+        
+        return res
+
