2b07d40f112f8ba8e5afc7c81d2727b3a52b3d38
Daanish Hindustani <134811343+Daanish-Hindustani@users.noreply.github.com>
2026-09-14 14:41:12 -0500

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        combine = []
+        st = []
+
+        for i in range(len(position)):
+            combine.append((position[i], speed[i]))
+        
+        combine.sort(reverse = True)
+
+        for p,s in combine:
+            st.append((target-p)/s)
+
+            if len(st) >= 2 and st[-2] >= st[-1]:
+                st.pop()
+        
+
+        return len(st)
